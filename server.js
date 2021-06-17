a2bf9016ea8372ff38fd92d7357279950fd7ea4a
Chuck Fields <info@spacetechcorp.com>
2021-06-17 11:27:13 -0400

    Connecting to db

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL Username
+        user: 'root',
+        // MySQL password
+        password: 'IgUa6406Ma!',
+        database: 'election'
+    },
+    console.log("Connected to the election database")
+);
+
+// test db connection
+db.query(`Select * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 // Test Route
 // app.get('/', (req, res) =>{
